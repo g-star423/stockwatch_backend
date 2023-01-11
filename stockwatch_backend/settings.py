20f4137ce40dcb76940daca0d38ca02989cd0c6b
Gregory Shoup <gregshoup@gmail.com>
2023-01-10 21:01:37 -0500

    added environmental variables

--- stockwatch_backend/settings.py
+++ stockwatch_backend/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-# import environ
+# import environmental variables helper
+import environ
 
-# env  = environ.Env()
+env = environ.Env()
 
-# environ.Env.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-goj_3@7&te+0di_of)g4r%(ou36e)wznz&hbev13@vg9susb!7"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
