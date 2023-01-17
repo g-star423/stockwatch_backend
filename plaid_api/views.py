a60a24937077c674b69be1c8e94d5866a0f5a496
Gregory Shoup <gregshoup@gmail.com>
2023-01-17 15:54:45 -0500

    able to exchange token and store in database

--- plaid_api/views.py
+++ plaid_api/views.py
@@ -17,7 +17,7 @@ environ.Env.read_env()
 import requests
 
 # to allow database manipulation
-import psycopg2
+from django.db import connection
 
 
 class UserTokenList(generics.ListCreateAPIView):
@@ -60,6 +60,8 @@ def exchange_token(request):
     if request.method == "POST":
         jsonRequest = json.loads(request.body)
 
+        user_id = jsonRequest["user_id"]
+
         plaid_exchange_request_body = {
             "client_id": env("CLIENT_ID"),
             "secret": env("SECRET_SANDBOX"),
@@ -70,7 +72,14 @@ def exchange_token(request):
             "https://sandbox.plaid.com/item/public_token/exchange",
             json=plaid_exchange_request_body,
         )
-        private_key = exchange_call["access_token"]
-        conn = psycopg2.connect("stockwatch_api")
-        cur = conn.cursor()
-        cur.execute(f"INSERT INTO plaid_api_usertoken VALUES ()")
+        exchange_call_json = exchange_call.json()
+        private_key = exchange_call_json["access_token"]
+        # conn = psycopg2.connect("stockwatch_api")
+        cur = connection.cursor()
+        cur.execute(
+            f"INSERT INTO plaid_api_usertoken (link_token, token_status, user_id_id) VALUES ('{private_key}', 'back from plaid OK', (SELECT id FROM auth_api_useraccount WHERE id={user_id}))"
+        )
+        # conn.commit()
+        # cur.close()
+        # conn.close()
+        return JsonResponse({"response": "hopefully ok"})
