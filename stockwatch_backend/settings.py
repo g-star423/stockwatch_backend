976eb84e72b0b10d0d8c9f38e8fff88a06428f05
Gregory Shoup <gregshoup@gmail.com>
2023-01-17 22:18:29 -0500

    typo in allowed hosts

--- stockwatch_backend/settings.py
+++ stockwatch_backend/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "boiling-crag-00382.herokuapp.com/"]
+ALLOWED_HOSTS = ["localhost", "boiling-crag-00382.herokuapp.com"]
 
 
 # Application definition
